ba2949199aa7bb8a4ccd232a94a22abba040cfb0
Lucas Camargo <lhcamargo3@gmail.comm>
2022-07-11 15:59:53 -0300

    Finalizado Chapter III

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,5 +1,6 @@
 import {ISpecificationRepository} from "../../repositories/ISpecificationRepository";
 import {inject, injectable} from "tsyringe";
+import {AppError} from "../../../../erros/AppError";
 
 interface IRequest{
     name: string;
@@ -20,7 +21,7 @@ class CreateSpecificationUseCase {
         const specificationAlreadyExists = await this.specificationsRepository.findByName(name);
 
         if(specificationAlreadyExists){
-            throw new Error("Specification already exists!");
+            throw new AppError("Specification already exists!");
         }
 
         await this.specificationsRepository.create({
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,6 +3,8 @@ import {ICategoriesRepository} from "../../modules/cars/repositories/ICategories
 import {CategoriesRepository} from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import {ISpecificationRepository} from "../../modules/cars/repositories/ISpecificationRepository";
 import {SpecificationsRepository} from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import {IUsersRepository} from "../../modules/accounts/repositories/IUsersRepository";
+import {UsersRepository} from "../../modules/accounts/repositories/implementations/UsersRepository";
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -12,4 +14,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
+)
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
 )
